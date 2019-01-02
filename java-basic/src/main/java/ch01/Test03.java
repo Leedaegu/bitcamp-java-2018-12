f658ba77e1d3e16a824f006e07ae734b6d3e1708
Leedaegu <dencw6@gmail.com>
2019-01-02 18:31:13 +0900

    eclipse변경사항저장

---
+++ java-basic/src/main/java/ch01/Test03.java
@@ -0,0 +1,17 @@
+
+
+package ch01;
+
+public class Test03 {
+
+  public String toString() {
+    return "ok";
+    /**
+    이 메서드는 Object 클래스의 메서드를 재정의한 것이다.
+     */
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  } 
+}
