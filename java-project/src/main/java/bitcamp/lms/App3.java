925a397dc97400c89bcf7869d03cd6df090c211f
Leedaegu <dencw6@gmail.com>
2019-01-04 20:15:46 +0900

    예제수정

--- java-project/src/main/java/bitcamp/lms/App3.java
+++ java-project/src/main/java/bitcamp/lms/App3.java
@@ -3,8 +3,18 @@ package bitcamp.lms;
 public class App3 {
   
     public static void main(String[] args) {
-        System.out.println("번호: 1");
-        System.out.println("내용: 게시글입니다.");
+      java.util.Scanner keyboard = new java.util.Scanner(System.in);
+      
+        System.out.print("번호? ");
+        int number = keyboard.nextInt();
+        keyboard.nextLine();
+        
+        System.out.print("내용? ");
+        String contents = keyboard.nextLine();
+        
+        System.out.println();
+        System.out.printf("번호: %d\n", number);
+        System.out.printf("내용: %s\n", contents);
         System.out.println("작성일: 2019-01-01");
         System.out.println("조회수: 0");
  
