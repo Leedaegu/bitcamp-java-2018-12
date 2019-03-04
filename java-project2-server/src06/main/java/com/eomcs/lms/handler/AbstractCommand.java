6e0a6564957cc8045a200eb6d557826e3c08eb31
Leedaegu <dencw6@gmail.com>
2019-03-04 18:18:13 +0900

    .

--- java-project2-server/src06/main/java/com/eomcs/lms/handler/AbstractCommand.java
+++ java-project2-server/src06/main/java/com/eomcs/lms/handler/AbstractCommand.java
@@ -2,29 +2,22 @@ package com.eomcs.lms.handler;
 
 import java.io.BufferedReader;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import com.eomcs.lms.ApplicationInitializer;
 
 // 추상 클래스의 목적?
 // => 서브 클래스에게 필드나 메서드를 상속해 주는 용도.
 // => 직접 사용하지 못한다.
 // 
 public abstract class AbstractCommand implements Command {
-
+  
   @Override
   public void execute(BufferedReader in, PrintWriter out) {
     try {
       execute(new Response(in, out));
     } catch (Exception e) {
-      try {
-        ApplicationInitializer.con.rollback();
-      } catch (SQLException e1) {
-        // 롤백하다가 발생한 예외는 더이상 처리할게 없다. 그냥 무시한다.
-      }
       out.printf("실행 오류! : %s\n", e.getMessage());
     }
   }
-
+  
   public void execute(Response response) throws Exception {
     // 서브 클래스에서 오버라이딩 해야할 메서드이다.
     // 그러나 서브 클래스가 반드시 오버라이딩 하도록 강요하지 않기 위해 추상 메서드로 선언하지 않는다.
