18f4df3b8e855b921d0e4c872a7577f9d9335a02
Leedaegu <dencw6@gmail.com>
2019-03-25 20:49:03 +0900

    .

--- java-project2-server/src/main/java/com/eomcs/lms/ServerApp.java
+++ java-project2-server/src/main/java/com/eomcs/lms/ServerApp.java
@@ -1,16 +1,8 @@
-// 28단계: LogJ 1.x 적용하기
-// => 애플리케이션을 실행하는 중에 내부 상태를 확인할 목적으로
-//    기록을 남기는 것을 "로깅(logging)" 이라 한다.
-// => 로깅은 애플리케이션을 실행하는 콘솔창으로 출력할 수도 있고
-//    파일이나 네트웍으로도 출력할 수 있다.
-// => 보통 실무에서는 파일로 기록을 남긴다.
-// => 로깅 작업을 도와주는 대표적인 라이브러리가 log4j 이다.
-//    출력 레벨에 따라 로깅을 조절할 수 있어 편리하다.
-//
+// 30단계: HTTP 프로토콜을 적용하여 클라이언트를 웹브라우저로 변경하기
+// 
 // 작업
-// 1) log4j 1.x 라이브러리를 추가한다.
-//     => mvnrepository.com에서 log4j 검색한다.
-//     => build.gradle에 라이브러리 추가한다.
+// 1) RequestHandlerThread 변경하기
+//    => HTTP 프로토콜에 따라서 클라이언트 요청을 읽고 응답한다.
 //
 package com.eomcs.lms;
 import java.io.BufferedReader;
@@ -25,10 +17,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.eomcs.lms.context.RequestMappingHandlerMapping;
 import com.eomcs.lms.context.RequestMappingHandlerMapping.RequestMappingHandler;
-import com.eomcs.lms.handler.Response;
+import com.eomcs.lms.handler.ServletRequest;
+import com.eomcs.lms.handler.ServletResponse;
 
 public class ServerApp {
-
   // 보통 클래스에서 사용할 로그 출력 객체는 클래스의 스태틱 멤버로 선언한다.
   final static Logger logger = LogManager.getLogger(ServerApp.class);
   
@@ -42,7 +34,7 @@ public class ServerApp {
 
     try (ServerSocket ss = new ServerSocket(8888)) {
       logger.info("서버 실행 중...");
-
+      
       // Spring IoC 컨테이너 준비
       iocContainer = new AnnotationConfigApplicationContext(AppConfig.class);
       printBeans();
@@ -94,17 +86,22 @@ public class ServerApp {
         String requestLine = in.readLine();
         logger.debug(requestLine);
         
-        while(true) {
-        String str = in.readLine();
-        if(str.length() == 0)
-          break; // 요청의 끝을 만나면 나간당
-        
+        while (true) {
+          String str = in.readLine();
+          if (str.length() == 0) // 요청의 끝을 만나면 읽기를 멈춘다. 
+            break;
         }
         
-        String commandPath = requestLine.split(" ")[1];
+        // 예) GET /member/list HTTP/1.1
+        // 예) GET /member/detail?no=10 HTTP/1.1
+        // 예) GET /member/add?name=aaa&email=aaa@test.com&password=1111 HTTP/1.1
+        // => requestURI[0] : /board/detail
+        // => requestURI[1] : no=1
+        String[] requestURI = requestLine.split(" ")[1].split("\\?");
+        String commandPath = requestURI[0];
+            
         // 클라이언트에게 응답하기
-        // => HTTP 프로토콜에 따라 응답헤더를 출력한다.
-        
+        // => HTTP 프로토콜에 따라 응답 헤더를 출력한다.
         
         // => 클라이언트 요청을 처리할 메서드를 꺼낸다.
         RequestMappingHandler requestHandler = handlerMapping.get(commandPath);
@@ -112,29 +109,37 @@ public class ServerApp {
         if (requestHandler == null) {
           out.println("HTTP/1.1 404 Not Found");
           out.println("Server: bitcamp");
-          out.println("Content-Type: text/html; charset=UTF-8");
+          out.println("Content-Type: text/plain; charset=UTF-8");
           out.println();
           out.println("실행할 수 없는 명령입니다.");
           out.flush();
           return;
         }
         
-        
         try {
+          // 요청을 처리할 메서드가 사용할 Request, Response 준비하기
+          ServletRequest request = new ServletRequest();
+          if (requestURI.length > 1) {
+            // 예) name=aaa&email=aaa@test.com&password=1111
+            request.setQueryString(requestURI[1]); 
+          }
+
+          ServletResponse response = new ServletResponse(in, out);
+          
+          // 클라이언트 요청을 처리할 메서드를 찾았다면 호출한다.
           out.println("HTTP/1.1 200 OK");
           out.println("Server: bitcamp");
           out.println("Content-Type: text/html; charset=UTF-8");
           out.println();
-          // 클라이언트 요청을 처리할 메서드를 찾았다면 호출한다.
+          
           requestHandler.method.invoke(
               requestHandler.bean, // 메서드를 호출할 때 사용할 인스턴스 
-              new Response(in, out)); // 메서드 파라미터 값
+              request, response); // 메서드 파라미터 값
           
         } catch (Exception e) {
           out.printf("실행 오류! : %s\n", e.getMessage());
           e.printStackTrace();
         }
-        
         out.flush();
         
       } catch (Exception e) {
@@ -144,18 +149,17 @@ public class ServerApp {
         e.printStackTrace(out);
         logger.error(strWriter.toString());
         
-        
-       
       }
       logger.info("클라이언트와 연결 종료.");
     }
   }
   
   private void printBeans() {
+    // 개발하는 동안 참고할 로그는 보통 debug 등급으로 출력한다.
     logger.debug("---------------------------------------------------"); 
     String[] names = iocContainer.getBeanDefinitionNames();
     for (String name : names) {
-      logger.debug(String.format("빈 생성 됨 (객체명%s, 클래스명=%s)", name, 
+      logger.debug(String.format("빈 생성 됨 (객체명=%s, 클래스명=%s)", name, 
           iocContainer.getBean(name).getClass().getName()));
     }
     logger.debug("---------------------------------------------------"); 
