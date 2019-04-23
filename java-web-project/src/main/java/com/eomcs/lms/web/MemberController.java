e9d1d1a16533c3b0cd41108d01e4fd85eb76fe5b
Leedaegu <dencw6@naver.com>
2019-04-23 20:54:33 +0900

    .

---
+++ java-web-project/src/main/java/com/eomcs/lms/web/MemberController.java
@@ -0,0 +1,95 @@
+package com.eomcs.lms.web;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.ServletContext;
+import javax.servlet.http.Part;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.MemberService;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+  @Autowired MemberService memberService;
+  @Autowired ServletContext servletContext;
+
+  @GetMapping("form")
+  public void form() {
+  }
+
+  @PostMapping("add")
+  public String add(Member member, Part photoFile) throws Exception {
+
+    if (photoFile.getSize() > 0) {
+      String filename = UUID.randomUUID().toString();
+      String uploadDir = servletContext.getRealPath(
+          "/upload/member");
+      photoFile.write(uploadDir + "/" + filename);
+      member.setPhoto(filename);
+    }
+
+    List<Member> members = memberService.list(null);
+    for (Member m : members) {
+      if (member.getEmail().equals(m.getEmail()))
+        return "member/error";
+    }
+    memberService.add(member);
+
+    return "redirect:.";
+  }
+
+  @GetMapping("delete/{no}")
+  public String delete(@PathVariable int no) throws Exception {
+
+    if (memberService.delete(no) == 0) 
+      throw new Exception("해당 번호의 회원이 없습니다.");
+
+    return "redirect:..";
+  }
+
+  @GetMapping("{no}")
+  public String detail(@PathVariable int no, Model model) throws Exception {
+
+    Member member = memberService.get(no);
+    model.addAttribute("member", member);
+    return "member/detail";
+  }
+
+  @RequestMapping
+  public String list(Model model) throws Exception {
+
+    List<Member> members = memberService.list(null);
+    model.addAttribute("list", members);
+    return "/member/list";
+  }
+
+  @GetMapping("search")
+  public void search(String keyword ,Model model) throws Exception {
+
+    List<Member> members = memberService.list(keyword);
+    model.addAttribute("list", members);
+  }
+
+  @PostMapping("update")
+  public String update(Member member, Part photoFile) throws Exception {
+
+    if (photoFile.getSize() > 0) {
+      String filename = UUID.randomUUID().toString();
+      String uploadDir = servletContext.getRealPath("/upload/member");
+      photoFile.write(uploadDir + "/" + filename);
+      member.setPhoto(filename);
+    }
+
+    if (memberService.update(member) == 0)
+      throw new Exception("해당 번호의 회원이 없습니다.");
+
+    return "redirect:.";
+  }
+}
---
+++ java-web-project/src/main/webapp/WEB-INF/jsp/member/error.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"
+  trimDirectiveWhitespaces="true"%>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Refresh" content="2;url=list">
+  <title>이메일 중복</title>
+</head>
+<body>
+  <jsp:include page="../header.jsp" />
+<h1>등록 실패</h1>
+<p>이메일이 중복되었습니다.</p>
+</body>
+</html>
